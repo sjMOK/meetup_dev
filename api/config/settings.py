db7f6e90c52df2102978808decde804f77148458
sjMOK <mokseungju@naver.com>
2023-03-30 15:41:23 +0900

    modify .env path settings

--- api/config/settings.py
+++ api/config/settings.py
@@ -12,16 +12,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
-import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# .env setting.
-env = environ.Env(DEBUG=(bool, True))
-environ.Env.read_env(
-    env_file=os.path.join(BASE_DIR.parent, '.env')
-)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
