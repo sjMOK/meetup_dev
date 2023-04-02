b7d8668df675eb70ef24295b741c73b9953fdfd1
sjMOK <mokseungju@naver.com>
2023-04-02 17:32:58 +0900

    add user_type field in User model and initiate migration file

--- api/users/models.py
+++ api/users/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.utils import timezone
 
 
@@ -8,7 +8,7 @@ class User(AbstractBaseUser):
     username = models.CharField(unique=True, max_length=45)
     name = models.CharField(max_length=45)
     email = models.EmailField()
-    # user_type = models.ForeignKey('UserType', models.DO_NOTHING)
+    user_type = models.ForeignKey('UserType', models.DO_NOTHING)
     is_active = models.BooleanField(default=True)
     date_joined = models.DateTimeField(default=timezone.now)
 
