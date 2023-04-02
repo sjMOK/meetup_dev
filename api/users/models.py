84edb6ec435e9dafd445df2da3c71d4ee175e091
sjMOK <mokseungju@naver.com>
2023-04-02 15:18:05 +0900

    create custom user model and initial migration file

--- api/users/models.py
+++ api/users/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.utils import timezone
 
-# Create your models here.
+
+class User(AbstractBaseUser):
+    id = models.AutoField(primary_key=True)
+    username = models.CharField(unique=True, max_length=45)
+    name = models.CharField(max_length=45)
+    email = models.EmailField()
+    # user_type = models.ForeignKey('UserType', models.DO_NOTHING)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(default=timezone.now)
+
+    USERNAME_FIELD = 'username'
+
+    # objects = CustomUserManager()
+
+    class Meta:
+        db_table = 'user'
