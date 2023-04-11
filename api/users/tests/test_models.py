196c67b7ad022cc4a8cae1b3544a8eb48a4f340e
sjMOK <mokseungju@naver.com>
2023-04-11 14:38:11 +0900

    write User model tests

--- api/users/tests/test_models.py
+++ api/users/tests/test_models.py
@@ -1,6 +1,41 @@
 from rest_framework.test import APITestCase
 
 # Create your tests here.
+class UserTestCase(APITestCase):
+    @classmethod
+    @freeze_time(FREEZE_TIME)
+    def setUpTestData(cls):
+        user_type = UserType.objects.create(id=1, name='admin', possible_duration=0)
+        cls.user = User.objects.create_user(username='username', password='password', name='name', email='email@sejong.ac.kr', user_type=user_type)
+
+    def test_pk_is_id(self):
+        pk_name = User._meta.pk.name
+        self.assertEqual(pk_name, 'id')
+
+    def test_username_is_unique(self):
+        is_unique = self.user._meta.get_field('username').unique
+        self.assertTrue(is_unique)
+
+    def test_username_max_length(self):
+        max_length = self.user._meta.get_field('username').max_length
+        self.assertEqual(max_length, 45)
+
+    def test_name_max_length(self):
+        max_length = self.user._meta.get_field('name').max_length
+        self.assertEqual(max_length, 45)
+
+    def test_is_active_default(self):
+        self.assertTrue(self.user.is_active)
+
+    @freeze_time(FREEZE_TIME)
+    def test_date_joined_default(self):
+        self.assertEqual(self.user.date_joined, timezone.now())
+
+    def test_username_field(self):
+        username_field = self.user.get_username()
+        self.assertEqual(username_field, 'username')
+
+
 class UserTypeTestCase(APITestCase):
     @classmethod
     def setUpTestData(cls):
