989f85c186112a9d5925751f03440ece4da65c04
sjMOK <mokseungju@naver.com>
2023-04-13 18:26:41 +0900

    create UserTypeSerializer and UserSerializer, and write tests

---
+++ api/users/serializers.py
@@ -0,0 +1,33 @@
+from rest_framework import serializers
+from rest_framework.exceptions import APIException
+
+from .models import User
+
+
+class UserTypeSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    name = serializers.CharField(read_only=True)
+    possible_duration = serializers.IntegerField(read_only=True)
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username', 'password', 'name', 'email', 'user_type']
+        extra_kwargs = {
+            'password': {
+                'write_only': True, 'required': False,
+            },
+        }
+
+    def to_representation(self, instance):
+        ret = super().to_representation(instance)
+        ret['user_type'] = UserTypeSerializer(instance.user_type).data
+
+        return ret
+
+    def validate_password(self, value):
+        if self.instance is not None:
+            raise APIException('Cannot update password with UserSerializer instance.')
+
+        return value
--- api/users/tests/test_serializers.py
+++ api/users/tests/test_serializers.py
@@ -1,3 +1,64 @@
 from rest_framework.test import APITestCase
+from rest_framework.exceptions import APIException
 
-# Create your tests here.
+from ..serializers import UserTypeSerializer, UserSerializer
+from ..models import UserType, User
+
+
+class UserTypeSerializerTestCase(APITestCase):
+    def test_all_fields_are_read_only(self):
+        serializer = UserTypeSerializer()
+        self.assertTrue(serializer.fields['id'].read_only)
+        self.assertTrue(serializer.fields['name'].read_only)
+        self.assertTrue(serializer.fields['possible_duration'].read_only)
+
+    def test_serialization(self):
+        user_type = UserType.objects.create(name='admin', possible_duration=0)
+        serializer = UserTypeSerializer(user_type)
+        expected_data = {
+            'id': user_type.id,
+            'name': user_type.name,
+            'possible_duration': user_type.possible_duration,
+        }
+
+        self.assertDictEqual(serializer.data, expected_data)
+
+
+class UserSerializerTestCase(APITestCase):
+    @classmethod
+    def setUpTestData(cls):
+        user_type = UserType.objects.create(name='admin', possible_duration=0)
+        cls.user = User.objects.create_user(username='username', password='password', name='name',
+                                        email='email@sejong.ac.kr', user_type=user_type)
+
+    def test_password_is_write_only(self):
+        serializer = UserSerializer()
+        self.assertTrue(serializer.fields['password'].write_only)
+
+    def test_password_required_false(self):
+        serializer = UserSerializer()
+        self.assertFalse(serializer.fields['password'].required)
+
+    def test_serialization(self):
+        serializer = UserSerializer(self.user)
+        expected_data = {
+            'username': self.user.username,
+            'name': self.user.name, 
+            'email': self.user.email,
+            'user_type': UserTypeSerializer(self.user.user_type).data,
+        }
+
+        self.assertDictEqual(serializer.data, expected_data)
+
+    def test_update_password_raise_error(self):
+        data = {
+            'password': 'update_password',
+        }
+        serializer = UserSerializer(self.user, data, partial=True)
+        
+        self.assertRaisesMessage(
+            APIException,
+            'Cannot update password with UserSerializer instance.',
+            serializer.is_valid,
+            raise_exception=True
+        )
