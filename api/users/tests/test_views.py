df899a79be6e71a246a8fa9783172f5f987a5115
sjMOK <mokseungju@naver.com>
2023-05-02 16:42:44 +0900

    create UserViewSet for user CRUD and write tests

--- api/users/tests/test_views.py
+++ api/users/tests/test_views.py
@@ -3,9 +3,10 @@ import json
 from django.contrib.sessions.backends.db import SessionStore
 
 from rest_framework.test import APITestCase
-from rest_framework.status import HTTP_200_OK, HTTP_404_NOT_FOUND, HTTP_400_BAD_REQUEST
+from rest_framework.status import HTTP_200_OK, HTTP_201_CREATED, HTTP_404_NOT_FOUND, HTTP_400_BAD_REQUEST, HTTP_403_FORBIDDEN
 
 from ..models import User, UserType
+from ..serializers import UserSerializer
 
 
 class LoginViewTestCase(APITestCase):
@@ -47,3 +48,170 @@ class LogoutViewTestCase(APITestCase):
         
         self.client.post('/users/logout')
         self.assertFalse(s.exists(session_id))
+
+
+class UserViewSetTestCase(APITestCase):
+    url = '/users'
+
+    @classmethod
+    def setUpTestData(cls):
+        admin_user_type = UserType.objects.create(id=1, name='admin', possible_duration=0)
+        cls.user_type = UserType.objects.create(name='undergraduate', possible_duration=1)
+
+        cls.admin_user = User.objects.create_user(username='admin', name='admin', email='admin@naver.com', password='password', user_type=admin_user_type)
+        cls.user = User.objects.create_user(username='user', name='user', email='user@naver.com', password='password', user_type=cls.user_type)
+
+    def test_get_by_admin_user(self):
+        self.url += f'/{self.user.id}'
+
+        self.client.force_authenticate(self.admin_user)
+        response = self.client.get(self.url)
+        body_data = json.loads(response.content)
+
+        self.assertEqual(response.status_code, HTTP_200_OK)
+        self.assertDictEqual(body_data, UserSerializer(self.user).data)
+
+    def test_get_by_normal_user(self):
+        self.url += f'/{self.user.id + 1}'
+
+        self.client.force_authenticate(self.user)
+        response = self.client.get(self.url)
+        body_data = json.loads(response.content)
+
+        self.assertEqual(response.status_code, HTTP_200_OK)
+        self.assertDictEqual(body_data, UserSerializer(self.user).data)
+
+    def test_get_by_anonymous_user(self):
+        self.url += f'/{self.user.id}'
+
+        self.client.force_authenticate(user=None)
+        response = self.client.get(self.url)
+
+        self.assertEqual(response.status_code, HTTP_403_FORBIDDEN)
+
+    def test_list_by_admin_user(self):
+        self.client.force_authenticate(self.admin_user)
+        response = self.client.get(self.url)
+        body_data = json.loads(response.content)
+
+        self.assertEqual(response.status_code, HTTP_200_OK)
+        self.assertListEqual(body_data, UserSerializer(User.objects.all(), many=True).data)
+
+    def test_list_by_normal_user(self):
+        self.client.force_authenticate(user=self.user)
+        response = self.client.get(self.url)
+
+        self.assertEqual(response.status_code, HTTP_403_FORBIDDEN)
+
+    def test_list_by_anonymous_user(self):
+        self.client.force_authenticate(user=None)
+        response = self.client.get(self.url)
+
+        self.assertEqual(response.status_code, HTTP_403_FORBIDDEN)
+
+    def test_create(self):
+        request_data = {
+            'username': 'newusername',
+            'password': 'password',
+            'name': 'newname',
+            'email': 'new@naver.com',
+            'user_type': self.user_type.id,
+        }
+        
+        self.client.force_authenticate(user=self.admin_user)
+        response = self.client.post(self.url, request_data, format='json')
+        created_user_id = response.get('Location')
+
+        self.assertEqual(response.status_code, HTTP_201_CREATED)
+        self.assertTrue(User.objects.filter(id=created_user_id).exists())
+
+    def test_create_by_normal_user(self):
+        self.client.force_authenticate(user=self.user)
+        response = self.client.post(self.url)
+
+        self.assertEqual(response.status_code, HTTP_403_FORBIDDEN)
+    
+    def test_create_by_anonymous_user(self):
+        self.client.force_authenticate(user=None)
+        response = self.client.post(self.url)
+
+        self.assertEqual(response.status_code, HTTP_403_FORBIDDEN)
+
+    def test_partial_update_by_admin_user(self):
+        self.url += f'/{self.user.id}'
+        reqeust_data = {'name': 'newname', 'email': 'new@naver.com'}
+
+        self.client.force_authenticate(user=self.admin_user)
+        response = self.client.patch(self.url, reqeust_data, format='json')
+        user = User.objects.get(id=self.user.id)
+
+        self.assertEqual(response.status_code, HTTP_200_OK)
+        self.assertEqual(user.name, reqeust_data['name'])
+        self.assertEqual(user.email, reqeust_data['email'])
+
+    def test_partial_update_by_normal_user(self):
+        self.url += f'/{self.user.id + 1}'
+        reqeust_data = {'name': 'newname', 'email': 'new@naver.com'}
+
+        self.client.force_authenticate(user=self.user)
+        response = self.client.patch(self.url, reqeust_data, format='json')
+        user = User.objects.get(id=self.user.id)
+
+        self.assertEqual(response.status_code, HTTP_200_OK)
+        self.assertEqual(user.name, reqeust_data['name'])
+        self.assertEqual(user.email, reqeust_data['email'])
+
+    def test_normal_user_try_to_update_non_patchable_field(self):
+        self.url += f'/{self.user.id}'
+        reqeust_data = {'username': 'newusername', 'name': 'newname', 'email': 'new@naver.com'}
+
+        self.client.force_authenticate(user=self.user)
+        response = self.client.patch(self.url, reqeust_data, format='json')
+        body_data = json.loads(response.content)
+
+        self.assertEqual(response.status_code, HTTP_400_BAD_REQUEST)
+        self.assertEqual(body_data, 'The data contains fields cannot be updated.')
+
+    def test_admin_user_try_to_update_non_patchable_field(self):
+        self.url += f'/{self.user.id}'
+        reqeust_data = {'username': 'newusername', 'name': 'newname', 'email': 'new@naver.com'}
+
+        self.client.force_authenticate(user=self.admin_user)
+        response = self.client.patch(self.url, reqeust_data, format='json')
+
+        self.assertEqual(response.status_code, HTTP_200_OK)
+
+    def test_partial_update_by_anonymous_user(self):
+        self.url += f'/{self.user.id}'
+        reqeust_data = {'name': 'newname', 'email': 'new@naver.com'}
+
+        self.client.force_authenticate(user=None)
+        response = self.client.patch(self.url, reqeust_data, format='json')
+
+        self.assertEqual(response.status_code, HTTP_403_FORBIDDEN)
+
+    def test_delete_by_admin_user(self):
+        self.url += f'/{self.user.id}'
+
+        self.client.force_authenticate(user=self.admin_user)
+        response = self.client.delete(self.url)
+
+        self.assertEqual(response.status_code, HTTP_200_OK)
+        self.assertFalse(User.objects.filter(id=self.user.id).exists())
+
+    def test_delete_by_normal_user(self):
+        self.url += f'/{self.user.id + 1}'
+
+        self.client.force_authenticate(user=self.user)
+        response = self.client.delete(self.url)
+
+        self.assertEqual(response.status_code, HTTP_200_OK)
+        self.assertFalse(User.objects.filter(id=self.user.id).exists())
+
+    def test_delete_by_anonymous_user(self):
+        self.url += f'/{self.user.id}'
+
+        self.client.force_authenticate(user=None)
+        response = self.client.delete(self.url)
+
+        self.assertEqual(response.status_code, HTTP_403_FORBIDDEN)
--- api/users/views.py
+++ api/users/views.py
@@ -3,8 +3,12 @@ from django.contrib.auth import login, authenticate, logout
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, authentication_classes
 from rest_framework.exceptions import NotFound
+from rest_framework.viewsets import GenericViewSet
+from rest_framework.status import HTTP_201_CREATED, HTTP_400_BAD_REQUEST
 
-from .serializers import LoginSerializer
+from .models import User
+from .serializers import LoginSerializer, UserSerializer
+from .permissions import UserAccessPermission
 
 
 @api_view(['POST'])
@@ -28,3 +32,51 @@ def login_view(request):
 def logout_view(request):
     logout(request)
     return Response('Logout success.')
+
+
+class UserViewSet(GenericViewSet):
+    __normal_user_patchable_fields = ('name', 'email')
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [UserAccessPermission]
+
+    def __get_user(self):
+        if self.request.user.is_admin():
+            return self.get_object()
+        return self.request.user
+
+    def __validate_data_contains_non_patchable_fields(self):
+        if not self.request.user.is_admin():
+            return set(self.request.data).difference(self.__normal_user_patchable_fields)
+        return False
+
+    def retrieve(self, request, pk):
+        user = self.__get_user()
+        serializer = self.get_serializer(user)
+        return Response(serializer.data)
+    
+    def list(self, request):
+        users = self.get_queryset()
+        serializer = self.get_serializer(users, many=True)
+        return Response(serializer.data)
+    
+    def create(self, request):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        return Response(status=HTTP_201_CREATED, headers={'Location': user.id})
+    
+    def partial_update(self, request, pk):        
+        if self.__validate_data_contains_non_patchable_fields():
+            return Response('The data contains fields cannot be updated.', status=HTTP_400_BAD_REQUEST)
+        user = self.__get_user()
+
+        serializer = self.get_serializer(user, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response()
+    
+    def destroy(self, request, pk):
+        user = self.__get_user()
+        user.delete()
+        return Response()
