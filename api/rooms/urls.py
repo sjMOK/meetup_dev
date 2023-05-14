2a5668676371c87e9d9feb25af1c43a4e3ed943d
zoe <ool2o8@github.com>
2023-05-14 16:53:57 +0900

    FEAT daily card 와 reservation 생성

--- api/rooms/urls.py
+++ api/rooms/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path
-from .views import RoomView, RoomDetailView
+from .views import (
+    ReservationRetrievView,
+    ReservationView,
+    RoomView,
+    DailyReservationCardView,
+)
 
 urlpatterns = [
     path("", RoomView.as_view({"get": "list", "post": "create"})),
     path(
-        "/<int:pk>",
-        RoomDetailView.as_view(
+        "<int:id>",
+        RoomView.as_view(
             {
                 "get": "retrieve",
                 "put": "update",
@@ -13,5 +18,35 @@ urlpatterns = [
                 "delete": "destroy",
             }
         ),
+    ),
+    path(
+        "/daily-reservation-card",
+        DailyReservationCardView.as_view({"get": "list", "post": "create"}),
+    ),
+    path(
+        "/daily-reservation-card/<int:pk>",
+        DailyReservationCardView.as_view(
+            {
+                "get": "retrieve",
+                "put": "update",
+                "patch": "partial_update",
+                "delete": "destroy",
+            }
+        ),
+    ),
+    path(
+        "/reservations/<int:pk>",
+        ReservationRetrievView.as_view(
+            {
+                "get": "retrieve",
+                "put": "update",
+                "patch": "partial_update",
+                "delete": "destroy",
+            }
+        ),
+    ),
+    path(
+        "/reservations/<int:card_id>/<str:time>",
+        ReservationView.as_view({"get": "list", "post": "create"}),
     ),
 ]
--- api/rooms/views.py
+++ api/rooms/views.py
@@ -1,8 +1,12 @@
 import json
 from rest_framework import viewsets
 from rest_framework.response import Response
-from .models import Room, RoomImages
-from .serializers import RoomSerializer
+from .models import DailyReservationCard, Reservation, Room, RoomImages
+from .serializers import (
+    DailyReservationCardSerializer,
+    ReservationSerializer,
+    RoomSerializer,
+)
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from datetime import datetime
 from django.views.decorators.csrf import csrf_exempt
@@ -18,11 +22,7 @@ class RoomView(viewsets.ModelViewSet):
     # permission_classes = [IsAuthenticated]
     queryset = Room.objects.all()
     serializer_class = RoomSerializer
-
-    def list(self, request):
-        queryset = Room.objects.all()
-        serializer = RoomSerializer(queryset, many=True)
-        return Response(serializer.data)
+    lookup_field = "id"
 
     def create(self, request):
         serializer = RoomSerializer(data=request.data)
@@ -35,7 +35,32 @@ class RoomView(viewsets.ModelViewSet):
         return Response({"message": "invalid form"})
 
 
-class RoomDetailView(viewsets.ModelViewSet):
-    # permission_classes = [IsAuthenticated]
-    queryset = Room.objects.all()
-    serializer_class = RoomSerializer
+class DailyReservationCardView(viewsets.ModelViewSet):
+    serializer_class = DailyReservationCardSerializer
+    queryset = DailyReservationCard.objects.all()
+
+
+class ReservationView(viewsets.ModelViewSet):
+    serializer_class = ReservationSerializer
+    lookup_field = "card_id", "time"
+    queryset = Reservation.objects.all()
+
+    def create(self, request, card_id, time):
+        serializer = ReservationSerializer(data=request.data)
+        queryset = DailyReservationCard.objects.filter(id=card_id).first()
+        if serializer.is_valid():
+            try:
+                serializer.save()
+                reserve = Reservation.objects.filter(id=serializer.data["id"]).first()
+                for selected_time in time.split("&"):
+                    setattr(queryset, selected_time, reserve)
+                queryset.save()
+                return Response({"message": "complete"})
+            except ValueError:
+                return Response({"message": "fail"})
+        return Response({"message": "invalid form"})
+
+
+class ReservationRetrievView(viewsets.ModelViewSet):
+    serializer_class = ReservationSerializer
+    queryset = Reservation.objects.all()
