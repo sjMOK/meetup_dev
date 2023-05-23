8c7d2ce19fcf78a3c7718c507849505ed8bd6584
sjMOK <mokseungju@naver.com>
2023-05-23 14:33:32 +0900

    refactor: 코드 위치 변경

--- api/users/views.py
+++ api/users/views.py
@@ -25,76 +25,6 @@ from .documentations import (
 )
 
 
-class UserCsvCreateView(APIView):
-    parser_classes = [MultiPartParser, PlainTextParser]
-
-    def validate(self, rdr, user_no_list):
-        error_occured = False
-
-        lines = []
-        validated_data_lst = []
-        for line in rdr:
-            if user_no_list.count(line['user_no']) > 1:
-                line['user_no_duplicated'] = True
-                error_occured = True
-
-            serializer = UserSerializer(data=line)
-            if serializer.is_valid():
-                validated_data_lst.append(serializer.validated_data)
-            else:
-                line['errors'] = serializer.errors
-                error_occured = True
-
-            lines.append(line)
-
-        return error_occured, lines, validated_data_lst
-
-    def get_attachment_response(self):
-        response = HttpResponse(content_type='text/csv')
-        response['Content-Disposition'] = 'attachment; filename="users.csv"'
-        response.write(u'\ufeff'.encode('utf8'))
-        return response
-
-    def write_csv(self, response, fieldnames, lines):
-        writer = csv.DictWriter(response, fieldnames=fieldnames)
-        writer.writeheader()
-        writer.writerows(lines)
-
-    def post(self, request, *args, **kwargs):
-        if 'user_data' not in request.FILES:
-            return Response("File key error(user_data).", HTTP_400_BAD_REQUEST)
-
-        file = request.FILES['user_data']
-        decoded_file = file.read().decode('euc-kr').splitlines()
-    
-        rdr = csv.reader(decoded_file)
-        user_no_list = [line[0] for line in list(rdr)[2:]]
-
-        file.seek(0)
-        dict_rdr = csv.DictReader(decoded_file)
-        next(dict_rdr)
-        error_occured, lines, validated_data_lst = self.validate(dict_rdr, user_no_list)
-
-        response = self.get_attachment_response()
-
-        fieldnames = dict_rdr.fieldnames + ['user_no_duplicated', 'errors']
-        self.write_csv(response, fieldnames, lines)
-
-        if not error_occured:
-            User.objects.bulk_create(
-                [User.objects.get_user_instance(**data) for data in validated_data_lst]
-            )
-
-        return response
-    
-    def delete(self, request, *args, **kwargs):
-        data_to_str = request.data.decode('utf-8')
-        user_no_lst = data_to_str.splitlines()
-
-        result = User.objects.filter(user_no__in=user_no_lst).delete()
-        return Response(result[1].get('users.User', 0))
-
-
 @swagger_auto_schema(method='POST', security=[], request_body=LoginSerializer, 
                      responses={200: UserResponse, 404: 'message: No matching user.\nbody의 user_no, password 틀렸을 때\n'}, 
                      operation_description=login_view_operation_description)
@@ -202,3 +132,73 @@ class UserViewSet(ModelViewSet):
     @action(detail=False, methods=['GET'], url_path='mine')
     def retrieve_mine(self, request, *args, **kwargs):
         return Response(UserSerializer(request.user).data)
+
+
+class UserCsvCreateView(APIView):
+    parser_classes = [MultiPartParser, PlainTextParser]
+
+    def validate(self, rdr, user_no_list):
+        error_occured = False
+
+        lines = []
+        validated_data_lst = []
+        for line in rdr:
+            if user_no_list.count(line['user_no']) > 1:
+                line['user_no_duplicated'] = True
+                error_occured = True
+
+            serializer = UserSerializer(data=line)
+            if serializer.is_valid():
+                validated_data_lst.append(serializer.validated_data)
+            else:
+                line['errors'] = serializer.errors
+                error_occured = True
+
+            lines.append(line)
+
+        return error_occured, lines, validated_data_lst
+
+    def get_attachment_response(self):
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename="users.csv"'
+        response.write(u'\ufeff'.encode('utf8'))
+        return response
+
+    def write_csv(self, response, fieldnames, lines):
+        writer = csv.DictWriter(response, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerows(lines)
+
+    def post(self, request, *args, **kwargs):
+        if 'user_data' not in request.FILES:
+            return Response("File key error(user_data).", HTTP_400_BAD_REQUEST)
+
+        file = request.FILES['user_data']
+        decoded_file = file.read().decode('euc-kr').splitlines()
+    
+        rdr = csv.reader(decoded_file)
+        user_no_list = [line[0] for line in list(rdr)[2:]]
+
+        file.seek(0)
+        dict_rdr = csv.DictReader(decoded_file)
+        next(dict_rdr)
+        error_occured, lines, validated_data_lst = self.validate(dict_rdr, user_no_list)
+
+        response = self.get_attachment_response()
+
+        fieldnames = dict_rdr.fieldnames + ['user_no_duplicated', 'errors']
+        self.write_csv(response, fieldnames, lines)
+
+        if not error_occured:
+            User.objects.bulk_create(
+                [User.objects.get_user_instance(**data) for data in validated_data_lst]
+            )
+
+        return response
+    
+    def delete(self, request, *args, **kwargs):
+        data_to_str = request.data.decode('utf-8')
+        user_no_lst = data_to_str.splitlines()
+
+        result = User.objects.filter(user_no__in=user_no_lst).delete()
+        return Response(result[1].get('users.User', 0))
