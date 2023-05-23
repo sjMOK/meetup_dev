c86895dceb9147e6a94ac4fe027dff07fc869b5f
sjMOK <mokseungju@naver.com>
2023-05-23 10:37:08 +0900

    user bulk create / delete api

--- api/users/urls.py
+++ api/users/urls.py
@@ -2,13 +2,12 @@ from django.urls import path
 
 from rest_framework.authtoken.views import obtain_auth_token
 
-from .views import login_view, logout_view, get_all_user_type, change_password
+from .views import get_all_user_type, change_password, UserCsvCreateView
 
 
 urlpatterns = [
-    # path('/login', login_view),
-    path('/logout', logout_view),
+    path('/login', obtain_auth_token),
     path('/types', get_all_user_type),
     path('/password', change_password),
-    path('/login', obtain_auth_token),
+    path('/bulk', UserCsvCreateView.as_view()),
 ]
