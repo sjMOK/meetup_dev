880cf1715d94da7c25433eb70a58314fd6ff8c42
sjMOK <mokseungju@naver.com>
2023-06-09 11:48:26 +0900

    email settings

--- api/config/settings.py
+++ api/config/settings.py
@@ -159,7 +159,8 @@ REST_FRAMEWORK = {
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.naver.com"
-EMAIL_USE_TLS = True
+# EMAIL_USE_TLS = True
+EMAIL_USE_SSL = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
